8607dcd67ec1f19a70f2e025a9142fd30d00ec87
hugoprodabel <138876687+hugoprodabel@users.noreply.github.com>
2023-07-07 08:49:02 -0300

    mais um commit app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("o paulo fez este commit sem vc ver na aula")
 
